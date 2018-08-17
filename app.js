8b912ada42cb74361da46da223e5ae39a08e2fc7
krntpm <42428731+krntpm@users.noreply.github.com>
2018-08-17 04:13:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -155,6 +155,21 @@ function handleMessage(sender_psid, received_message) {
     // Send the response message
     callSendAPI(sender_psid, response);
 }
+function handlePostback(sender_psid, received_postback) {
+  console.log('ok')
+   let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
 
 // Define the template and webview
 function setRoomPreferences(sender_psid) {
