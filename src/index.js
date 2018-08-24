a457a59ff88433c2a1d513c0f97f0c1c8ec5a353
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-24 03:20:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ var _server = require('./server');
 
 var _server2 = _interopRequireDefault(_server);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { 
+     console.log("=================START JA==============");
+    return obj && obj.__esModule ? obj : { default: obj }; 
+}
 
 _server2.default.bootstrap();
